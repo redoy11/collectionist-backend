fea37b9f42d3b142262e2f3d0280341c57492d7d
Nafiz Ahmed <redoyahmed11@gmail.com>
2021-02-12 15:50:29 +0600

    Integrate collections routes

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var authenticateRouter = require('./routes/authenticate')
+var authenticateRouter = require('./routes/authenticate');
+var collectionsRouter = require('./routes/collections');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/authorize', authenticateRouter);
+app.use('/collections', collectionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
