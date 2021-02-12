9038a09de32641f0be4b308206996a7746806dd2
Nafiz Ahmed <redoyahmed11@gmail.com>
2021-02-12 18:41:58 +0600

    Set ssl to true in uri

--- routes/repos.js
+++ routes/repos.js
@@ -5,8 +5,7 @@ const Pool = require('pg').Pool;
 
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  connectionString: process.env.DATABASE_URL + `?ssl=true`,
 });
 
 // list the repos based on users
