f5383c3eea2d7018dd4f41b2c48f08aa415e5cfa
Nafiz Ahmed <redoyahmed11@gmail.com>
2021-02-12 16:09:08 +0600

    Integrate the routes of repos

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authenticateRouter = require('./routes/authenticate');
 var collectionsRouter = require('./routes/collections');
+var reposRouter = require('./routes/repos');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/authorize', authenticateRouter);
 app.use('/collections', collectionsRouter);
+app.use('/repos', reposRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
