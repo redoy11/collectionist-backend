fdd8b50743d48f118ade1bd3462a49f82b0999b6
Nafiz Ahmed <redoyahmed11@gmail.com>
2021-02-12 17:51:52 +0600

    Refactor database connection using uri

--- routes/repos.js
+++ routes/repos.js
@@ -5,11 +5,7 @@ const Pool = require('pg').Pool;
 
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 // list the repos based on users
